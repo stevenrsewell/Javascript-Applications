bc00442011f6663e74f38499c42c997acee7dc94
Steven Sewell <stevenrsewell@outlook.com>
2023-03-25 23:48:08 -0400

    Updated calculator to accept text input also

--- Calculator/script.js
+++ Calculator/script.js
@@ -13,3 +13,33 @@ function calculate() {
   let result = eval(expression);
   display.innerHTML = result;
 }
+
+document.addEventListener("keydown", function(event) {
+  let keyCode = event.keyCode;
+  let key = event.key;
+  
+  // If the user types a number, append it to the display
+  if (keyCode >= 48 && keyCode <= 57) {
+    appendToDisplay(key);
+  }
+  
+  // If the user types a decimal point, append it to the display
+  if (keyCode === 190 || keyCode === 110) {
+    appendToDisplay(".");
+  }
+  
+  // If the user types an operator, append it to the display
+  if (key === "+" || key === "-" || key === "*" || key === "/") {
+    appendToDisplay(key);
+  }
+  
+  // If the user types "Enter", calculate the result
+  if (keyCode === 13) {
+    calculate();
+  }
+  
+  // If the user types "Backspace", clear the display
+  if (keyCode === 8) {
+    clearDisplay();
+  }
+});
