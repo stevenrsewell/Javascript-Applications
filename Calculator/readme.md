1b8dd2eec44d27ae481c3be6bb275b055cb59c0b
Steven Sewell <stevenrsewell@outlook.com>
2023-03-27 19:46:51 -0400

    Update To Do list to include categories

---
+++ Calculator/readme.md
@@ -0,0 +1,3 @@
+# Calculator
+
+Javascript application that allows users to input their own numbers or use the buttons on the calculator to perform addition, subtraction, multiplication, and division. 
--- To Do List/script.js
+++ To Do List/script.js
@@ -1,20 +1,36 @@
 const form = document.querySelector('#todo-form');
 const input = document.querySelector('#todo-input');
+const categoryInput = document.querySelector('#category-input');
 const list = document.querySelector('#todo-list');
 
 form.addEventListener('submit', (e) => {
-  e.preventDefault(); // Prevent form submission
-  const todoText = input.value.trim(); // Remove whitespace from input value
-  if (todoText !== '') {
+  e.preventDefault();
+  const todoText = input.value.trim();
+  const categoryText = categoryInput.value.trim();
+  if (todoText !== '' && categoryText !== '') {
     const todoItem = document.createElement('li');
-    todoItem.innerText = todoText;
+    todoItem.classList.add('list-group-item');
+    todoItem.innerHTML = `
+      <div class="d-flex justify-content-between align-items-center">
+        <div>
+          <span class="mr-2">${todoText}</span>
+          <span class="badge badge-primary badge-pill">${categoryText}</span>
+        </div>
+        <button type="button" class="close" aria-label="Remove">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+    `;
     list.appendChild(todoItem);
-    input.value = ''; // Clear input field
+    input.value = '';
+    categoryInput.value = '';
   }
 });
 
 list.addEventListener('click', (e) => {
-  if (e.target.tagName === 'LI') {
+  if (e.target.classList.contains('close')) {
+    e.target.parentElement.parentElement.remove();
+  } else if (e.target.tagName === 'SPAN' && !e.target.classList.contains('badge')) {
     e.target.classList.toggle('completed');
   }
 });
